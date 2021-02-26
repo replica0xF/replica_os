b130f22c601eba1f65f308fd6e031764df1f8427
replica0xf <replica0xf@protonmail.com>
2021-02-25 19:06:49 -0500

    updated images and did some UI work

--- kernel/System/panic.c
+++ kernel/System/panic.c
@@ -5,21 +5,27 @@ void kernel_panic(char* message , int err_code)
 	asm("cli");
 	VGE_SetBackgroundColor(  rgb2hex(100,44,44));	
 	VGE_ClearBG();
-	VGE_ClearCursorPos();
-
-	VGE_PrintString("\n\n\n" , 7, rgb2hex(255,0,0));
-	
-
-	VGE_PrintString("Fatal error occured.\n" , 7, rgb2hex(255,0,0));
-	VGE_PrintString( "Message:" , 7, rgb2hex(255,255 /2,255/2));
-	VGE_PrintString(message , 7, rgb2hex(255,255,255));
-	VGE_PrintString("\nERROR:CODE: ", 7, rgb2hex(255,255,255));
-	VGE_PrintString(int2str(err_code), 7, rgb2hex(255,255,255));
-
- 	VGE_Rectangle( 0 , (uint32_t)fb_hdr_tag->framebuffer_height - 70, (uint32_t)fb_hdr_tag->framebuffer_width, 70 , rgb2hex(200, 10,10));
-	VGE_Rectangle(  0 , (uint32_t)fb_hdr_tag->framebuffer_height - 70, (uint32_t)fb_hdr_tag->framebuffer_width / 2, 70 , rgb2hex(255,10,10));
-	
-	VGE_PrintStringPos( "Reboot your system to continue.", 7, rgb2hex(255,255,255), CreateVec2( (uint32_t)fb_hdr_tag->framebuffer_height / 2, (uint32_t)fb_hdr_tag->framebuffer_width / 2));
+ 
+ 
+  	VGE_SetY(50);
+ 
+ 	VGE_PrintStringPos("ReplicaOS : Kernel Module" , 8,  rgb2hex(255,150,150) , CreateVec2(0,25));
+ 	VGE_PrintString("FATAL PANIC.\n" , 8 , rgb2hex(255,150,150));
+	VGE_Rectangle(0,VGE_GetY(),fb_hdr_tag->framebuffer_width,2,rgb2hex(255,100,100));
+  	VGE_PrintString("\nOops! FATAL CRASH. Your computer has suffered a kernel panic.\nThis may be easily fix by rebooting your system.\n" , 8 , rgb2hex(255,255,255));
+	VGE_Rectangle(0,VGE_GetY(),fb_hdr_tag->framebuffer_width,2,rgb2hex(255,100,100));
+ 
+ 	VGE_PrintString("\nAdditonal Information:\n" , 8 , rgb2hex(255,255,255));
+ 	VGE_PrintString(message , 8 , rgb2hex(255,255,255));
+ 	VGE_PrintString("\n" , 8 , rgb2hex(255,255,255));
+ 	VGE_PrintString("Error Code:  " , 8 , rgb2hex(255,255,255));
+ 	VGE_PrintString(int2str(err_code), 8 , rgb2hex(255,255,255));
+ 	VGE_PrintString("\n", 8 , rgb2hex(255,255,255));
+	 	
+ 	
+ 
+ 
+ 
 
 	while (1);
 }
--- kernel/main.c
+++ kernel/main.c
@@ -6,13 +6,13 @@
 #include "System/Interupts/IDT.h"
 #include "../lobster/logger.h"
 
+static uint8_t ui_ver = 2;
 void kernel_main(struct stivale2_struct* stivale2_struct)
 {
 	
   
 	Boot_LocateTag(stivale2_struct);
-  
-  	InitGDT(); 
+   	InitGDT(); 
  	Initialize_IDT();
 
  	VGE_SetBackgroundColor(rgb2hex(20,20,20));
@@ -21,16 +21,15 @@ void kernel_main(struct stivale2_struct* stivale2_struct)
   	VGE_SetXp(5);
   	VGE_SetYp(5);
   	VGE_SetY(50);
-  	VGE_Rectangle(  fb_hdr_tag->framebuffer_width- 50 ,  fb_hdr_tag->framebuffer_height - 50 , 50 , 50 , rgb2hex(255,0,0));
-	VGE_Rectangle(  fb_hdr_tag->framebuffer_width- 100,  fb_hdr_tag->framebuffer_height - 50, 50 , 50 , rgb2hex(0,255,0));
-	VGE_Rectangle(  fb_hdr_tag->framebuffer_width- 150,  fb_hdr_tag->framebuffer_height - 50, 50 , 50 , rgb2hex(0,0,255));
-	VGE_Rectangle(0,100,100,100,rgb2hex(100,100,100));
+  	VGE_Rectangle(  fb_hdr_tag->framebuffer_width- 25 ,  fb_hdr_tag->framebuffer_height - 25 , 25 , 25 , rgb2hex(255,0,0));
+	VGE_Rectangle(  fb_hdr_tag->framebuffer_width- 50,  fb_hdr_tag->framebuffer_height - 25, 25 , 25 , rgb2hex(0,255,0));
+	VGE_Rectangle(  fb_hdr_tag->framebuffer_width- 75,  fb_hdr_tag->framebuffer_height - 25, 25 , 50 , rgb2hex(0,0,255));
  	VGE_PrintStringPos("ReplicaOS" , 8, rgb2hex(160,170,255) , CreateVec2(0,25));
  	VGE_PrintString("IDT, GDT Loaded.\n" , 8 , rgb2hex(255,255,255));
+	VGE_Rectangle(0,VGE_GetY(),fb_hdr_tag->framebuffer_width,2,rgb2hex(100,100,100));
+ 	VGE_PrintString("\n(c) 2021: ReplicaOS Foundation\n" , 8 , rgb2hex(255,255,255));
 
-
-   
-
+ 	
  
  
   	Boot_Loop();
